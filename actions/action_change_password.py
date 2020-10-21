0efafa9326d176d4fd6013be5fcbb1d30c5a766c
root <root@june-test.novalocal>
2020-10-21 09:15:33 +0000

    Corrected the rules and actions code

--- actions/action_change_password.py
+++ actions/action_change_password.py
@@ -4,4 +4,5 @@ from st2common.runners.base_action import Action
 
 class ChangePassword(Action):
   def run(self, passw_daysleft):
-    print(" Your password is going to be expired in " + str(passw_daysleft) + " days."
+    print(" Your password is going to be expired in " + str(passw_daysleft) + " days.")  
+    return(True, "Succesfully executed action")
